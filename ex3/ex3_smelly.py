d3452ca9aeae1371d415a0d0b0328eb844aa32bf
chester <chesterwong.2022@scis.smu.edu.sg>
2024-11-28 02:27:57 +0800

    added missing product line in smell file

--- ex3/ex3_smelly.py
+++ ex3/ex3_smelly.py
@@ -23,7 +23,7 @@ class Clothing(Product):
     def __init__(self, name, price):
         super().__init__(name, price, discount=0.2, tax_rate=0.08)
 
-class Grocery:
+class Grocery(Product):
     def __init__(self, name, price):
         super().__init__(name, price, discount=0.05, tax_rate=0.02)
 
